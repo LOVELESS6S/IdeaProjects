8fc133af05f8e7c9ff12a616e49e49b36155995e
BladeRunner683 <aa897127446@gmail.com>
2024-10-15 17:00:58 +0800

    updated version, used two loops and is able to calculate multiple numbers

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -1,58 +1,63 @@
-import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
         while (true) {
-            System.out.print("type exit to exit, or type any other key to continue: ");
+            System.out.print("Type 'exit' to exit, or type any other key to continue: ");
+            String userInput = input.nextLine();
+
+            if (userInput.equalsIgnoreCase("exit")) {
+                break;
+            }
+
             ArrayList<Double> numberList = new ArrayList<>();
             ArrayList<String> operatorList = new ArrayList<>();
-            Scanner input = new Scanner(System.in);
-            double sum = numberList.getFirst();
 
-            if (input.nextLine().equals("exit")) {
-                break;
-            } else {
-                while (true) {
-                    System.out.println("insert number.");
-                    numberList.add(input.nextDouble());
+            System.out.println("Insert the first number:");
+            numberList.add(input.nextDouble()); // Add the first number
 
-                    System.out.println("insert operator.");
-                    String operator = input.next();
+            while (true) {
+                System.out.println("Insert operator (+, -, *, /) or '=' to calculate:");
+                String operator = input.next();
+
+                if (operator.equals("=")) {
+                    break;
+                } else if (operator.matches("[+\\-*/]")) {
                     operatorList.add(operator);
-                    if (operatorList.equals("=")) {
-                        for (int i = 0; i < operatorList.size(); i++) {
-                            switch (operatorList.get(i)) {
-                                case "+":
-                                    sum += numberList.get(i+1);
-                                    break;
 
-                                case "-":
-                                    sum -= numberList.get(i+1);
-                                    break;
-
-                                case "*":
-                                    sum *= numberList.get(i+1);
-                                    break;
-
-                                case "/":
-                                    sum /= numberList.get(i+1);
-                                    break;
-
-                                default:
-                                    System.out.println("nut a valid operator");
-                            }
-                            }
-
-                        }
-                        else {
-                            operatorList.add(operator);
-                            System.out.println(sum);
-                            break;
-                        }
-                    }
+                    System.out.println("Insert the next number:");
+                    numberList.add(input.nextDouble());
+                } else {
+                    System.out.println("Not a valid operator. Please enter +, -, *, / or =.");
                 }
             }
-        }
-    }
 
+
+            double sum = numberList.getFirst();
+            for (int i = 0; i < operatorList.size(); i++) {
+                switch (operatorList.get(i)) {
+                    case "+":
+                        sum += numberList.get(i + 1);
+                        break;
+                    case "-":
+                        sum -= numberList.get(i + 1);
+                        break;
+                    case "*":
+                        sum *= numberList.get(i + 1);
+                        break;
+                    case "/":
+                        sum /= numberList.get(i + 1);
+                        break;
+                }
+            }
+
+            System.out.println("Result: " + sum);
+            input.nextLine();
+        }
+
+        input.close();
+    }
+}
